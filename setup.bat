31f0e9093a37de672909729e1844ecf7d2498b21
Bluemangoo <chenfy2006@qq.com>
2022-06-19 11:14:00 +0800

    setup add yarn

--- setup.bat
+++ setup.bat
@@ -3,5 +3,21 @@
 @rem by Bluemangoo
 @rem https://github.com/Bluemangoo/hexotemplate
 echo HEXO template install script
+
+:checkyarn
+yarn > nul || goto npm
+goto yarn
+
+:npm
 npm install
 npm install hexo -g
+goto end
+
+:yarn
+yarn install
+yarn global add hexo
+goto end
+
+:end
+echo 安装完成!
+pause
